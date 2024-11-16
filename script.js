4fb746638da55f3eb1bfef8bef639971f9571045
kanika-07cs <kanikaab.cs24@bitsathy.ac.in>
2024-11-16 21:08:22 +0530

    changed

---
+++ script.js
@@ -0,0 +1,19 @@
+/* alert('hello');
+console.log(3+5);
+console.log('hello '+'world'); */
+
+/* let showQuantity=0;
+let updateQuantity=showQuantity+1; */
+
+/* const age=30;
+if(age>=18){
+    console.log('You can drive');
+}
+else{
+    console.log('You can not drive');
+} */
+
+function sample(){
+    console.log('hello');
+}
+sample();
