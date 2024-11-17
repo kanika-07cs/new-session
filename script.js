44fc03579a2eb71710759931d7c24934bfc28bac
kanika-07cs <kanikaab.cs24@bitsathy.ac.in>
2024-11-17 20:52:29 +0530

    modified

--- script.js
+++ script.js
@@ -13,7 +13,21 @@ else{
     console.log('You can not drive');
 } */
 
-function sample(){
+/* function sample(){
     console.log('hello');
 }
-sample();
+sample(); */
+
+let computerMove ; // global scope
+function PickcomputerMove(){
+    const randomNumber=Math.random();
+      if(randomNumber>=0 && randomNumber<1/3){
+        computerMove='Rock';
+      }
+      else if(randomNumber>=1/3 && randomNumber<2/3){
+         computerMove='Paper';;
+      }
+      else if(randomNumber>=2/3 && randomNumber<1){
+         computerMove='Scissors';
+      }
+}
